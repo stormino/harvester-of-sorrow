95a238e24144ffed5d8956c3c19381ace75745fd
stormino <stormino@gmail.com>
2026-03-21 15:27:54 +0100

    Show version on app

--- src/main/java/com/github/stormino/view/MainLayout.java
+++ src/main/java/com/github/stormino/view/MainLayout.java
@@ -9,26 +9,34 @@ import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.sidenav.SideNav;
 import com.vaadin.flow.component.sidenav.SideNavItem;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.vaadin.flow.spring.annotation.UIScope;
 import com.vaadin.flow.theme.lumo.LumoUtility;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * Main application layout with sidebar navigation
  */
+@SpringComponent
+@UIScope
 public class MainLayout extends AppLayout {
-    
-    public MainLayout() {
+
+    private final String appVersion;
+
+    public MainLayout(@Value("${app.version:unknown}") String appVersion) {
+        this.appVersion = appVersion;
         createHeader();
         createDrawer();
     }
-    
+
     private void createHeader() {
         H1 appName = new H1("VixSrc Downloader");
         appName.addClassNames(
                 LumoUtility.FontSize.LARGE,
                 LumoUtility.Margin.NONE
         );
-        
-        Span version = new Span("v1.0.0");
+
+        Span version = new Span("v" + appVersion);
         version.addClassNames(
                 LumoUtility.FontSize.SMALL,
                 LumoUtility.TextColor.SECONDARY
